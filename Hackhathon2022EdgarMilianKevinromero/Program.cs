a7e2b2f7485eb5667935f0ab5fa6ca8d339e4654
Sebastian Milian <milianmejiaedgarsebastian@yahoo.com>
2022-11-05 14:34:23 -0600

    inicio del proyecto

---
+++ Hackhathon2022EdgarMilianKevinromero/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hackhathon2022EdgarMilianKevinromero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Hackhathon2022EdgarMilianKevinromero/Programas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackhathon2022EdgarMilianKevinromero
+{
+    internal class Programas
+    {
+        public bool Problema1 (string CadenaOriginal, string Cadena)
+        {
+
+            char [] Caracteres = CadenaOriginal.ToCharArray();
+            
+            return true;
+        }
+    }
+}
